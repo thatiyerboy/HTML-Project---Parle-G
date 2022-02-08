c42eba2e69bcc78120bb5972ac0a39a6647ac6dd
Shashank Iyer <97697626+thatiyerboy@users.noreply.github.com>
2022-02-07 23:17:41 -0500

    Add files via upload

---
+++ Final Project/js/custom-js.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+  //Loads in the header
+  $(function(){
+    $("#navigation").load("./js/header.html");
+  });
+
+  //Loads in the footer
+  $(function(){
+    $("#footer").load("./js/footer.html");
+  });
+});
